be448385247618e9f6515ef366f0c40101355c3f
asadmash <mdasad.mash@gmail.com>
2024-12-17 15:00:11 +0600

    cta button implemented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,9 @@ useEffect(() => {
 }, [])
 
   useGSAP(() => {
-gsap.to("#hero", { opacity: 1.5, delay: 1.5 })
+gsap.to("#hero", { opacity: 1, delay: 2 })
+// animate the cta button and text
+gsap.to('#cta', {opacity: 1, y: -50, delay: 2})
   }, []);
   return (
     <section className="w-full nav-height bg-black relative">
@@ -40,6 +42,11 @@ gsap.to("#hero", { opacity: 1.5, delay: 1.5 })
   <source src={videoSrc} type='video/mp4'/>
 </video>
 </div>
+</div>
+{/* Call to action button */}
+<div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
+<a href="#highlights" className='btn'>Buy</a>
+<p className='font-normal text-xl'>From $199/month or $999</p>
 </div>
     </section>
   )
