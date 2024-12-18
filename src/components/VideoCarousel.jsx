3cd6dc4130b367f95d189f04d23424cd760679e2
asadmash <mdasad.mash@gmail.com>
2024-12-18 16:04:02 +0600

    video carousel play/pause and progress bar design and functionality implemented

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,24 +1,124 @@
 // import React from 'react'
+import { useRef } from "react";
 import { highlightsSlides } from "../constants";
+import { useState } from "react";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { pauseImg, playImg, replayImg } from "../utils";
 
 const VideoCarousel = () => {
+  // video references
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  // loaded data state
+  const [loadedData, setLoadedData] = useState([]);
+
+  // destructure values from the useState
+  const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+
+  // useEffect hook for video playing
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
+
+  // useEffect hook for video animation progress
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
+
+    if (span[videoId]) {
+      // animate the progress of the video
+      let anim = gsap.to(span[videoId], {
+        onUpdate: () => {},
+        onComplete: () => {},
+      });
+    }
+  }, [videoId, startPlay]);
   return (
     <>
       {/* container of the carousel */}
       <div className="flex items-center">
         {/* map thought the highlightsSlides array comming from index.js */}
-        {highlightsSlides.map((list) => (
+        {highlightsSlides.map((list, i) => (
           // return something
           <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
               <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                <video id="video" playsInline={true} preload="auto" muted>
+                <video
+                  id="video"
+                  playsInline={true}
+                  preload="auto"
+                  muted
+                  ref={(el) => (videoRef.current[i] = el)}
+                  onPlay={() => {
+                    setVideo((prevVideo) => ({
+                      ...prevVideo,
+                      isPlaying: true,
+                    }));
+                  }}
+                >
                   <source src={list.video} type="video/mp4" />
                 </video>
+              </div>
+
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
               </div>
             </div>
           </div>
         ))}
+      </div>
+      {/* STEP: 01 -> CREATE A DIV TO CONTAIN THE CONTROL BUTTONS */}
+      <div className="relative flex-center mt-10">
+        {/* STEP: 02 -> ANOTHER CHILD DIV */}
+        <div className="flex-center py-5 px-7 bg-gray-300 background-blur rounded-full">
+          {videoRef.current.map((_, i) => (
+            <span
+              key={i}
+              ref={(el) => (videoDivRef.current[i] = el)}
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+            >
+              <span
+                className="absolute h-full w-full rounded-full"
+                ref={(el) => (videoSpanRef.current[i] = el)}
+              />
+            </span>
+          ))}
+        </div>
+        {/* STEP: 03 -> BUTTON */}
+        <button className="control-btn">
+          <img
+            src={isLastVideo ? replayImg : !isPlaying ? playImg : pauseImg}
+            alt={isLastVideo ? "replay" : !isPlaying ? "play" : "pause"}
+            onClick={
+              isLastVideo
+                ? () => handleProcess("video-reset")
+                : !isPlaying
+                ? () => handleProcess("play")
+                : () => handleProcess("pause")
+            }
+          />
+        </button>
       </div>
     </>
   );
