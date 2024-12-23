74a80cb7b98646fac2ba231f1a015ae98cffd38b
asadmash <mdasad.mash@gmail.com>
2024-12-23 15:37:38 +0600

    Add ModelView component with conditional styling

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,15 @@
+import { View } from '@react-three/drei'
 import React from 'react'
 
 const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+    index={index}
+    id={gsapType}
+    className= {`border-2 border-red-500 w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+
+    </View>
   )
 }
 
