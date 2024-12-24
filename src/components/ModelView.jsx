1c2a98a707fd00a4c060b6e092572c8a29468d95
asadmash <mdasad.mash@gmail.com>
2024-12-24 16:51:15 +0600

    Iphone model rendered in ModelView

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,24 +1,37 @@
-import { PerspectiveCamera, View } from '@react-three/drei'
-import React from 'react'
-import Lights from './Lights'
-import { Suspense } from 'react'
+import { Html, PerspectiveCamera, View } from "@react-three/drei";
+import React from "react";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import Model from "./Iphone";
 
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
   return (
     <View
-    index={index}
-    id={gsapType}
-    className= {`border-2 border-red-500 w-full h-full ${index === 2 ? 'right-[-100%]' : ''}`}
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2 ? "right-[-100%]" : ""
+      }`}
     >
-{/* ambient light */}
-<ambientLight intensity={0.3} />
-<PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+      {/* ambient light */}
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
-<Lights/>
-{/* suspense component */}
-<Suspense fallback={<div>Loading...</div>}></Suspense>
+      <Lights />
+      {/* suspense component */}
+      <Suspense fallback={<Html><div>Loading...</div></Html>}>
+        <Model />
+      </Suspense>
     </View>
-  )
-}
+  );
+};
 
-export default ModelView
+export default ModelView;
