5831a1bb13f38c5c9e6cb62798bd3071880703dc
asadmash <mdasad.mash@gmail.com>
2025-01-01 14:54:41 +0600

    feature section created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Highlights from './components/Highlights';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Model from './components/Model';
+import Features from './components/Features';
+
 import * as Sentry from '@sentry/react';
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
 <Hero/>
 <Highlights/>
 <Model/>
+<Features/>
     </main>
   )
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <Section>Features</Section>
+  )
+}
+
+export default Features
