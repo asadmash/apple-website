d94c798d753c41a782caa6e8aaf14ef0ae12ae83
asadmash <mdasad.mash@gmail.com>
2025-01-05 14:36:09 +0600

    feature section text

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -70,11 +70,6 @@ const Features = () => {
                 </div>
               </div>
 
-              <div className="feature-video-container">
-                <div className="overflow-hidden flex1 h-[50vh]">
-                  <img src={explore1Img} alt="" />
-                </div>
-              </div>
 
               <div className="feature-text-container">
                 <div className="flex-1 flex-center">
@@ -86,6 +81,15 @@ const Features = () => {
                     </span>
                     , using the same alloy that spacecrafts use for missions to
                     Mars.
+                  </p>
+                </div>
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of any metal, making these our{" "}
+                    <span className="text-white">
+                     lightest Pro model ever. 
+                    </span>
+                    {" "} You'll notice the difference the moment you pick one up.
                   </p>
                 </div>
               </div>
